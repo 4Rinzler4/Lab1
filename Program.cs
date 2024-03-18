949075320fb3664b566cccb7881c8b839a2cb409
4Rinzler4 <emaillubomyr103@gmail.com>
2024-03-18 11:27:36 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class DigitSumAndProduct
+{
+    public static (int sum, int product) CalculateSumAndProduct(int number)
+    {
+        int sum = 0;
+        int product = 1;
+
+        while (number > 0)
+        {
+            int digit = number % 10;
+            sum += digit;
+            product *= digit;
+            number /= 10;
+        }
+        return (sum, product);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть 4-значне число");
+        int d = int.Parse(Console.ReadLine());
+        (int sum, int product) = DigitSumAndProduct.CalculateSumAndProduct(d);
+        Console.WriteLine("Сума цифр: " + sum);
+        Console.WriteLine("Добуток цифр: " + product);   
+    }
+}
